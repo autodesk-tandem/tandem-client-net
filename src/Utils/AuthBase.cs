ad99924613b822027e8eb040a9b33e7949b5b7a0
Jan Liska <jan.liska@autodesk.com>
2023-07-26 14:31:07 +0200

    added authentication helper

---
+++ src/Utils/AuthBase.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+
+namespace Autodesk.Tandem.Client.Utils
+{
+    public class AuthToken
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; }
+
+        [JsonProperty("expires_in")]
+        public int ExpiresIn { get; set; }
+    }
+
+    public abstract class AuthBase
+    {
+        public AuthBase(string clientID, string clientSecret, string scope)
+        {
+            ClientID = clientID;
+            ClientSecret = clientSecret;
+            Scope = scope;
+            HttpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://developer.api.autodesk.com")
+            };
+        }
+
+        protected string ClientID { get; private set; }
+        protected string ClientSecret { get; private set; }
+        protected string Scope { get; private set; }
+
+        protected HttpClient HttpClient { get; private set; }
+
+        abstract protected FormUrlEncodedContent GetContent();
+
+        public async Task<T?> GetTokenAsync<T>()
+        {
+            var req = new HttpRequestMessage(HttpMethod.Post, "authentication/v2/token");
+            var auth = $"{ClientID}:{ClientSecret}";
+
+            req.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic",
+                Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(auth)));
+            req.Content = GetContent();
+            var response = await HttpClient.SendAsync(req);
+            var stream = await response.Content.ReadAsStreamAsync();
+            using var streamReader = new StreamReader(stream);
+            using var jsonReader = new JsonTextReader(streamReader);
+            var serializer = new JsonSerializer();
+            var result = serializer.Deserialize<T>(jsonReader);
+
+            return result;
+        }
+    }
+}
---
+++ src/Utils/TwoLeggedAuth.cs
@@ -0,0 +1,39 @@
+﻿namespace Autodesk.Tandem.Client.Utils
+{
+    public class TwoLeggedAuth : AuthBase
+    {
+        private string accessToken;
+        private DateTime expirationTime;
+
+        public TwoLeggedAuth(string clientID, string clientSecret, string scope)
+            : base(clientID, clientSecret, scope)
+        {
+        }
+
+        public string GetToken()
+        {
+            if (string.IsNullOrEmpty(accessToken) || ((expirationTime - DateTime.Now).TotalSeconds < 10))
+            {
+                var result = GetTokenAsync<AuthToken>().ConfigureAwait(false).GetAwaiter().GetResult();
+
+                if (result == null)
+                {
+                    return string.Empty;
+                }
+                expirationTime = DateTime.Now.AddSeconds(result.ExpiresIn);
+                accessToken = result.AccessToken;
+            }
+            return accessToken;
+        }
+
+        protected override FormUrlEncodedContent GetContent()
+        {
+            var fields = new List<KeyValuePair<string, string>>
+        {
+            new KeyValuePair<string, string>("grant_type", "client_credentials"),
+            new KeyValuePair<string, string>("scope", Scope)
+        };
+            return new FormUrlEncodedContent(fields);
+        }
+    }
+}
